e85f3f6d38bc71eb08dd4939c7e68d52b327d8f6
Tim Harper <timcharper@gmail.com>
2008-02-04 17:54:22 -0700

    Git stash commands

---
+++ Support/lib/commands/stash.rb
@@ -0,0 +1,46 @@
+require ENV['TM_SUPPORT_PATH'] + '/lib/ui.rb'
+
+class SCM::Git::Stash
+  include SCM::Git::CommonCommands
+  def initialize()
+    Dir.chdir(git_base)
+  end
+  
+  def stashes
+    @stashes = command("stash", "list").split("\n").map do |line|
+      /^(.+?):(.+)$/.match(line)
+      {:name => $1, :description => $2}
+    end
+  end
+  
+  def stash
+    command("stash")
+  end
+  
+  def stash_diff(name)
+    SCM::Git::Diff.parse_diff(command("stash", "show", "-p", name))
+  end
+  
+  def clear_with_confirmation
+    response = TextMate::UI.alert(:warning, "Clear all stashes?", "Do you really want to clear the following stashes? \n#{stashes.map{|s| s[:name]} * "\n"}", 'Yes', 'Cancel') 
+    if response == 'Yes'
+      clear
+      true
+    else
+      false
+    end
+  end
+  
+  def clear
+    command("stash", "clear")
+  end
+  
+  def select_stash(options={})
+    options = {:title => "Select stash", :prompt => "Select a stash", :items => stashes.map{|s| "#{s[:name]} - #{s[:description]}"}}.merge(options)
+    TextMate::UI.request_item(options) do |name|
+      return name
+    end
+    
+    nil
+  end
+end
