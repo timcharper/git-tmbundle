c07685e8c77943f28c9890b418bbf864605ae8f4
Tim Harper <timcharper@gmail.com>
2008-02-22 00:00:31 -0700

    bugfix: make compatible with older versions of erb

--- Support/lib/formatters.rb
+++ Support/lib/formatters.rb
@@ -3,7 +3,7 @@ require 'erb'
 class ERBStdout < ERB
   def set_eoutvar(compiler, eoutvar = 'STDOUT')
     compiler.put_cmd = "#{eoutvar} << "
-    compiler.insert_cmd = "#{eoutvar} << "
+    compiler.insert_cmd = "#{eoutvar} << " if compiler.respond_to?(:insert_cmd)
     compiler.pre_cmd = "#{eoutvar}.flush"
     compiler.post_cmd = "#{eoutvar}.flush; ''"
   end
