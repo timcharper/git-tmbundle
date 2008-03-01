0edc40a616139bbb26ae78a241f376c98b06fce6
Tim Harper <timcharper@gmail.com>
2008-03-01 14:37:16 -0700

    created string helper to help with autoloading / filename conventions

---
+++ Support/lib/string.rb
@@ -0,0 +1,13 @@
+class String
+  def constantize
+    eval(self)
+  end
+  
+  def underscore
+    gsub(/(^|\b)[A-Z]/) { |l| l.downcase}.gsub(/[A-Z]/) { |l| "_#{l.downcase}" }.gsub("::", "/")
+  end
+  
+  def classify
+    gsub(/^[a-z]/) { |l| l.upcase}.gsub(/_[a-z]/) { |l| l[1..1].upcase}.gsub(/\b[a-z]/) {|l| l.upcase}.gsub("/", "::")
+  end
+end
