5900c3d5c4aa1956672de5b22c688d158cdfd973
Tim Harper <timcharper@gmail.com>
2008-03-08 03:40:24 -0700

    compare branches working again

--- Support/app/controllers/diff_controller.rb
+++ Support/app/controllers/diff_controller.rb
@@ -1,6 +1,7 @@
 class DiffController < ApplicationController
   
   def diff
+    show_diff_title unless params[:layout].to_s=="false"
     @rev = params[:rev]
     @title = params[:title] || "Uncomitted changes"
     render("_diff_results", :locals => {:diff_results => git.diff(params)})
@@ -27,6 +28,17 @@ protected
     puts <<-EOF
       <a href='txmt://open?url=file://#{e_url '/tmp/output.diff'}'>Open diff in TextMate</a>
     EOF
+  end
+  
+  def show_diff_title
+    puts "<h2>"
+    case
+    when params[:branches]
+      branches = params[:branches]
+      branches = branches.split("..") if params[:branches].is_a?(String)
+      puts "Comparing branches #{branches.first}..#{branches.last}"
+    end
+    puts "</h2>"
   end
 
 end
