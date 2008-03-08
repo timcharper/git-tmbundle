6db9e252f18b2b696ef7157344b9c67d54cc3e2f
Tim Harper <timcharper@gmail.com>
2008-03-08 04:05:06 -0700

    git_spec needed updating

--- Support/spec/lib/git_spec.rb
+++ Support/spec/lib/git_spec.rb
@@ -4,20 +4,21 @@ describe SCM::Git do
   before(:each) do
     @git = SCM::Git.new
   end
+  
   it "should retrieve a value" do
-    @git.command_output << "../origin"
+    Git.command_output << "../origin"
     @git.config["remote.origin.url"].should == "../origin"
   end
   
   it "should set a value" do
-    @git.command_output << "../origin"
-    @git.command_output << "../origin"
+    Git.command_output << "../origin"
+    Git.command_output << "../origin"
     @git.config["remote.origin.url"] = "../origin"
     @git.config["remote.origin.url"].should == "../origin"
   end
   
   it "should return nil on blank, non-existing value" do
-    @git.command_output << ""
+    Git.command_output << ""
     @git.config["remote.origin.url"].should be_nil
   end
   
@@ -26,12 +27,12 @@ describe SCM::Git do
     end
     
     it "should get the right prefix for an origin" do
-      @git.command_output << "+refs/heads/*:refs/remotes/satellite/*"
-      @git.remote_branch_prefix("satellite").should == "satellite"
+      Git.command_output << "+refs/heads/*:refs/remotes/satellite/*"
+      @git.branch.remote_branch_prefix("satellite").should == "satellite"
     end
     
     it "should retrieve all remote branches for a given origin" do
-      @git.command_output << <<-EOF
+      Git.command_output << <<-EOF
   asdf
 * master
   mybranch
@@ -43,9 +44,9 @@ describe SCM::Git do
   satellite/mybranch
   satellite/satellite
       EOF
-      @git.command_output << "+refs/heads/*:refs/remotes/satellite/*"
+      Git.command_output << "+refs/heads/*:refs/remotes/satellite/*"
       
-      branches = @git.branches(:remote, :remote_name => "satellite")
+      branches = @git.branch.list(:remote, :remote_name => "satellite")
       branches.map{|r|r[:name]}.should == ["satellite/asdf", "satellite/master", "satellite/mybranch", "satellite/satellite"]
     end
   end
