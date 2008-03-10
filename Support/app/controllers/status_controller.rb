d4f85b67c116c49ff271fdd5b4a892a50da1a9cf
Tim Harper <timcharper@gmail.com>
2008-03-10 10:22:19 -0600

    removed "fix me" notice (it was fixed).  status listing on merge commit was being filtered down to the selected file or directory, when it should show the status for the whole project (since it's doing a whole commit)

--- Support/app/controllers/status_controller.rb
+++ Support/app/controllers/status_controller.rb
@@ -1,6 +1,6 @@
 class StatusController < ApplicationController
   def index
-    file_or_path = params[:file_or_path] || git.paths.first
+    file_or_path = params[:path] || git.paths.first
     puts '<h2>Status for ' + file_or_path.map { |e| "‘#{htmlize(shorten(e))}’" }.join(', ') + '</h2>'
     @status_data = git.status(file_or_path)
     render "_status", :locals => {:status_data => @status_data}
