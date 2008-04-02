6ffae36d90c1d52279ccf04262e7f2183bfbc15a
Tim Harper <timcharper@gmail.com>
2008-04-02 13:29:27 -0600

    spec to check proper default-module name extraction

--- Support/spec/controllers/submodule_controller_spec.rb
+++ Support/spec/controllers/submodule_controller_spec.rb
@@ -7,29 +7,32 @@ describe SubmoduleController do
   before(:each) do
     @git = Git.singleton_new
     @controller = SubmoduleController.singleton_new
+    
+    @module_repo_path = "git@server:/path/to/my-module.git"
+    @module_name = "my-module"
   end
   
   # TextMate::UI.should_receive(:request_string).with(
   #   :title => "Add submodule", :prompt => "Enter the submodule clone URL"
-  # ).and_return(module_repo_path)
+  # ).and_return(@module_repo_path)
   
   # TextMate::UI.should_receive(:request_directory).with(
   #   "Select the parent folder for the submodule:", :initial_directory => @git.git_base
   # ).and_return("/base/")
   
-  # TextMate::UI.should_receive(:request_string).with(
-  #   :title => "What do you want to call the module (will be the folder name)?", :default => "my-module"
-  # ).and_return(module_name)
+  it "should extract an intelligent default" do
+    TextMate::UI.should_receive(:request_string).with(
+      :title => "What do you want to call the module (will be the folder name)?", :default => "my-module"
+    ).and_return(@module_name)
+    @controller.send(:prompt_module_name, @module_repo_path)
+  end
   
   it "should add a repository" do
-    module_repo_path = "git@server:/path/to/my-module.git"
-    module_name = "my-module"
-    
-    @controller.should_receive(:prompt_repository_path).and_return(module_repo_path)
+    @controller.should_receive(:prompt_repository_path).and_return(@module_repo_path)
     @controller.should_receive(:prompt_parent_folder).and_return(@git.git_base)
-    @controller.should_receive(:prompt_module_name).and_return(module_name)
+    @controller.should_receive(:prompt_module_name).and_return(@module_name)
     
-    @git.submodule.should_receive(:add).with(module_repo_path, File.join("/base/", module_name)).and_return(StringIO.new("Added!"))
+    @git.submodule.should_receive(:add).with(@module_repo_path, File.join("/base/", @module_name)).and_return(StringIO.new("Added!"))
     @git.submodule.should_receive(:init_and_update).and_return("")
     
     output = capture_output do
