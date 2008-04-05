31f8985a7d986b64c1fac10cab1dd2fb27f2a7f4
Tim Harper <timcharper@gmail.com>
2008-04-04 22:45:11 -0600

    git.config can work with local or remote variables

---
+++ Support/spec/lib/commands/config_spec.rb
@@ -0,0 +1,63 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe SCM::Git do
+  before(:each) do
+    @git = SCM::Git.new
+    Git.reset_mock!
+  end
+  
+  it "should let git decide when local/global not specified" do
+    Git.command_response["config", "remote.origin.url"] = "../origin"
+    value = @git.config["remote.origin.url"]
+    Git.commands_ran.first.should == ["config", "remote.origin.url"]
+    value.should == "../origin"
+  end
+
+  it "should default to local when writing" do
+    Git.command_response["config", "remote.origin.url", "../origin"] = ""
+    @git.config["remote.origin.url"] = "../origin"
+    Git.commands_ran.first.should == ["config", "remote.origin.url", "../origin"]
+  end
+
+  it "should allow reading of local values" do
+    Git.command_response["config", "--file", "/base/.git/config", "remote.origin.url"] = "../origin"
+    value = @git.config[:local, "remote.origin.url"]
+    Git.commands_ran.first.should == ["config", "--file", "/base/.git/config", "remote.origin.url"]
+    value.should == "../origin"
+  end
+
+  it "should allow writing of local values" do
+    Git.command_response["config", "--file", "/base/.git/config", "remote.origin.url", "../origin"] = ""
+    @git.config[:local, "remote.origin.url"] = "../origin"
+    Git.commands_ran.first.should == ["config", "--file", "/base/.git/config", "remote.origin.url", "../origin"]
+  end
+  
+  
+  it "should allow reading global values" do
+    Git.command_response["config", "--global", "remote.origin.url"] = "../origin"
+    value = @git.config[:global, "remote.origin.url"]
+    Git.commands_ran.first.should == ["config", "--global", "remote.origin.url"]
+    value.should == "../origin"
+  end
+  
+  it "should raise when given a scope it doesn't understand" do
+    lambda { @git.config[:boogy, "remote.origin.url"] }.should raise_error("I don't understand the scope :boogy")
+  end  
+  
+  it "should respond to the string version of global as well as the symbol" do
+    Git.command_response["config", "--global", "remote.origin.url"] = "../origin"
+    @git.config[:global, "remote.origin.url"].should == "../origin"
+    @git.config["global", "remote.origin.url"].should == "../origin"
+  end
+  
+  it "should allow writing global values" do
+    Git.command_response["config", "--global", "remote.origin.url", "../origin"] = ""
+    @git.config[:global, "remote.origin.url"] = "../origin"
+    Git.commands_ran.first.should == ["config", "--global", "remote.origin.url", "../origin"]
+  end
+
+  it "should return nil on blank, non-existing value" do
+    Git.command_output << ""
+    @git.config["remote.origin.url"].should be_nil
+  end
+end
