09c221d5ac214e12c89befffdf8dd5f2d89fb200
Tim Harper <timcharper@gmail.com>
2008-04-16 18:12:09 -0600

    moved gitk command into the misc controller.  Added a delayed "Wish Shell" activate hack that helps solve the problem of gitk launching into the background

--- Support/app/controllers/misc_controller.rb
+++ Support/app/controllers/misc_controller.rb
@@ -3,4 +3,27 @@ class MiscController < ApplicationController
     puts "<h2>Initializing Git Repository in #{ENV['TM_PROJECT_DIRECTORY']}</h2>"
     puts htmlize(git.init(ENV["TM_PROJECT_DIRECTORY"]))
   end
+  
+  def gitk
+    exit if fork            # Parent exits, child continues.
+    Process.setsid          # Become session leader.
+    exit if fork            # Zap session leader.
+
+    # After this point you are in a daemon process
+    fork do
+      
+      STDOUT.reopen(open('/dev/null'))
+      STDERR.reopen(open('/dev/null'))
+      Dir.chdir(ENV['TM_PROJECT_DIRECTORY'])
+      Thread.new do
+        sleep 0.05
+        %x{osascript -e 'tell app "Wish Shell" to activate'}
+      end
+      %x{gitk --all}
+    end
+
+    Process.detach(pid)
+
+    #copied from http://andrejserafim.wordpress.com/2007/12/16/multiple-threads-and-processes-in-ruby/
+  end
 end
