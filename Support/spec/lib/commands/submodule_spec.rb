52da7a2ed9da59b5aa93f5baed3eca8c5194d97e
Tim Harper <timcharper@gmail.com>
2008-04-17 02:11:12 -0600

    bugfix - (+) is a valid sign for a submodule, indicating that the revision is currently ahead of what it should be

--- Support/spec/lib/commands/submodule_spec.rb
+++ Support/spec/lib/commands/submodule_spec.rb
@@ -11,18 +11,23 @@ describe SCM::Git::Submodule do
   it "should return all submodules when listing one" do
     Git.command_response["submodule"] = <<EOF
 -03a20cfe2e0e344f87ac3132ddc991899cef2567 fixture-scenarios
++23f26dec3851f50dbd4f7a132735b962b436898e great-module
  e741e43171fd34c98ef98afa5877fb2d74841b82 mod (undefined)
 EOF
     submodules = @git.submodule.all
-    submodules.should have(2).submodules
+    submodules.should have(3).submodules
     
     submodules[0].revision.should == "03a20cfe2e0e344f87ac3132ddc991899cef2567"
     submodules[0].name.should == "fixture-scenarios"
     submodules[0].tag.should be_nil
-    
-    submodules[1].revision.should == "e741e43171fd34c98ef98afa5877fb2d74841b82"
-    submodules[1].name.should == "mod"
+
+    submodules[1].revision.should == "23f26dec3851f50dbd4f7a132735b962b436898e"
+    submodules[1].name.should == "great-module"
     submodules[1].tag.should be_nil
+    
+    submodules[2].revision.should == "e741e43171fd34c98ef98afa5877fb2d74841b82"
+    submodules[2].name.should == "mod"
+    submodules[2].tag.should be_nil
   end
   
   it "should add a repository" do
