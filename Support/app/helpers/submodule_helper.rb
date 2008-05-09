f1f87dd21727c5288f1844e0d3f0cf28a45ee047
Tim Harper <timcharper@gmail.com>
2008-05-08 23:40:37 -0600

    condensed with_submodule_cacheing method

--- Support/app/helpers/submodule_helper.rb
+++ Support/app/helpers/submodule_helper.rb
@@ -1,19 +1,15 @@
 module SubmoduleHelper
   module Update
-    flush
     def with_submodule_cacheing(&block)
       git.submodule.all.each { |m| m.cache }
       begin
         yield
       ensure
-        git.submodule.all.each { |m| m.restore }
+        new_modules = git.submodule.all
+        new_modules.each { |m| m.restore }
       end
       
-      update_submodules_si_hay
-    end
-    
-    def update_submodules_si_hay
-      unless git.submodule.all.empty?
+      unless new_modules.empty?
         puts "<br /><br /><h3>Updating submodules</h3>"
         puts htmlize(git.submodule.init_and_update)
       end
