0c48dedf212f9e020db64aba844f253fe051d05a
Tim Harper <timcharper@gmail.com>
2008-05-09 00:28:11 -0600

    cleanup branch command. lazy caching of branch variables.

--- Support/spec/lib/commands/branch_spec.rb
+++ Support/spec/lib/commands/branch_spec.rb
@@ -5,6 +5,7 @@ describe SCM::Git::Branch do
     @git = Git.new
     Git.reset_mock!
   end
+  
   include SpecHelpers
   
   it "should list ignore (no branch)" do
@@ -14,5 +15,31 @@ describe SCM::Git::Branch do
 EOF
     
     @git.branch.list_names.should == ["test_mod"]
+  end
+  
+  describe "listing branches" do
+    before(:each) do
+      Git.command_response["branch"] = <<EOF
+* test_mod
+  master
+EOF
+      Git.command_response["branch", "-r"] = <<EOF
+  origin/alpha
+  origin/test_mod
+  origin/master
+EOF
+    end
+    
+    it "should return a list of branches" do
+      @git.branch.all.should have(5).branches
+    end
+    
+    it "should filter to the remote branches" do
+      @git.branch.all(:remote).should have(3).branches
+    end
+    
+    it "should filter to the local branches" do
+      @git.branch.all(:local).should have(2).branches
+    end
   end
 end
