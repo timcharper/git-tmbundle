1a2aeb15827005b7a309af3414f3b2bff98f902d
Tim Harper <timcharper@gmail.com>
2008-05-21 00:28:51 -0600

    spec to check submodule pushing, and fixed some other specs too

--- Support/spec/controllers/remote_controller_spec.rb
+++ Support/spec/controllers/remote_controller_spec.rb
@@ -7,19 +7,16 @@ describe RemoteController do
   before(:each) do
     Git.reset_mock!
     @git = Git.singleton_new
-    @git.config.stub!(:[]).with("branch.master.remote").and_return("origin")
     @git.config.stub!(:[]).with("git-tmbundle.log.limit").and_return(5)
+    @git.config.stub!(:[]).with("branch.master.remote").and_return("origin")
+    stub_current_branch(@git, :name => "refs/heads/master", :remote => "origin")
+    @git.config
   end
   
   describe "fetching" do
     before(:each) do
       # query the remotes
-      Git.command_response["branch"] = "* master\n"
-      Git.command_response["config", "branch.master.remote"] = %Q{origin}
-      Git.command_response["remote"] = %Q{origin}
-      
-      git = Git.singleton_new
-      git.should_receive(:log).with(:path=>".", :revisions=>["74c0fdf", "d1c6bdd"], :limit => 5).and_return(parse_log(fixture_file("log_with_diffs.txt")))
+      @git.should_receive(:log).with(:path=>".", :revisions=>["74c0fdf", "d1c6bdd"], :limit => 5, :git_path => @git.path).and_return(parse_log(fixture_file("log_with_diffs.txt")))
       
       Git.command_response["fetch", "origin"] = fixture_file("fetch_1_5_4_3_output.txt")
       
@@ -43,18 +40,13 @@ describe RemoteController do
     before(:each) do
       # query the remotes
       @controller = RemoteController.singleton_new
-      Git.command_response["branch"] = "* master\n"
-      Git.command_response["branch", "-r"] = "  origin/master\n  origin/release\n"
-      @git.config.stub!(:[]).with("remote.origin.fetch").and_return("+refs/heads/*:refs/remotes/origin/*")
-      @git.config.stub!(:[]).with("branch.master.merge").and_return("refs/heads/master")
-      Git.command_response["remote"] = %Q{origin}
-    
-      # query the config - if remote != self["remote.#{current_branch}.remote"] || self["remote.#{current_branch}.merge"].nil?
+      @git.branch.current.stub!(:merge).and_return("refs/heads/master")
+      # @git.config.stub!(:[]).with("remote.origin.fetch").and_return("+refs/heads/*:refs/remotes/origin/*")
     
       # Git.command_response[] 
       Git.command_response["log", "-p", "791a587..4bfc230", "."] = fixture_file("log_with_diffs.txt")
       Git.command_response["log", "-p", "dc29d3d..05f9ad9", "."] = fixture_file("log_with_diffs.txt")
-      Git.command_response["pull", "origin"] = fixture_file("pull_1_5_4_3_output.txt")
+      Git.command_response["pull", "origin", "master"] = fixture_file("pull_1_5_4_3_output.txt")
     end
     
     it "should output log of changes pulled" do
@@ -80,16 +72,17 @@ describe RemoteController do
       Git.command_response["log", ".", "865f920..f9ca10d"] = fixture_file("log.txt")
     end
     
-    describe "to a server with one origin" do
+    describe "to a server with one origin and no submodules" do
       before(:each) do
-        Git.command_response["remote"] = %Q{origin}
+        @git.remote.stub!(:names).and_return(['origin'])
+        @git.submodule.stub!(:all).and_return([])
         @output = capture_output do
           dispatch :controller => "remote", :action => "push"
         end
       end
       
       it "should run all git commands" do
-        Git.commands_ran.should == [["branch"], ["remote"], ["push", "origin", "master"], ["log", "-n", 5, "865f920..f9ca10d", "."], ["branch"], ["branch"]]
+        Git.commands_ran.should == [["push", "origin", "master"], ["log", "-n", 5, "865f920..f9ca10d", "."]]
       end
       
       it "should output log with diffs" do
@@ -99,6 +92,21 @@ describe RemoteController do
       
       it "should render the script on the top" do
         (Hpricot(@output) / "head / script").length.should >= 2
+      end
+    end
+    
+    it "should push all submodules that are behind" do
+      @behind_submodule = stub("submodule", :path => "behind", :git => stub("git", :path => "behind", :branch => stub("branch_command", :current => stub("branch", :remote_name => "origin", :name => "master", :tracking_branch_name => "master", :tracking_status => :behind))))
+      @ahead_submodule  = stub("submodule", :path => "ahead",  :git => stub("git", :path => "ahead" , :branch => stub("branch_command", :current => stub("branch", :remote_name => "origin", :name => "master", :tracking_branch_name => "master", :tracking_status => :ahead))))
+      @git.submodule.stub!(:all).and_return([@behind_submodule, @ahead_submodule])
+      
+      @ahead_submodule.git.should_receive(:push).and_return("")
+      @behind_submodule.git.should_not_receive(:push)
+      
+      @controller = RemoteController.singleton_new
+      @controller.stub!(:display_push_output)
+      @output = capture_output do
+        dispatch :controller => "remote", :action => "push"
       end
     end
   end
@@ -114,7 +122,7 @@ describe RemoteController do
     end
     
     it "should call run_push and then display_push_output" do
-      @controller.should_receive(:run_push).with("origin", :tag => "mytag")
+      @controller.should_receive(:run_push).with(@git, "origin", :tag => "mytag")
       @controller.should_receive(:display_push_output)
       @output = capture_output do
         dispatch(:controller => "remote", :action => "push_tag", :tag => "mytag")
