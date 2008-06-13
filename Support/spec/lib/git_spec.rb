0e663f9094997354197bc308909267d33a2d9f7f
Tim Harper <timcharper@gmail.com>
2008-06-13 15:20:40 -0600

    these specs are obsolete

--- Support/spec/lib/git_spec.rb
+++ Support/spec/lib/git_spec.rb
@@ -6,35 +6,6 @@ describe SCM::Git do
     Git.reset_mock!
   end
   
-  describe "when getting branches" do
-    before(:each) do
-    end
-    
-    it "should get the right prefix for an origin" do
-      Git.command_output << "+refs/heads/*:refs/remotes/satellite/*"
-      @git.branch.remote_branch_prefix("satellite").should == "satellite"
-    end
-    
-    it "should retrieve all remote branches for a given origin" do
-      Git.command_output << <<-EOF
-  asdf
-* master
-  mybranch
-  satellite
-  origin/master
-  origin/mybranch
-  satellite/asdf
-  satellite/master
-  satellite/mybranch
-  satellite/satellite
-      EOF
-      Git.command_output << "+refs/heads/*:refs/remotes/satellite/*"
-      
-      branches = @git.branch.list(:remote, :remote => "satellite")
-      branches.map{|r|r[:name]}.should == ["satellite/asdf", "satellite/master", "satellite/mybranch", "satellite/satellite"]
-    end
-  end
-  
   it "should describe a revision, defaulting to use all refs" do
     Git.command_response["describe", "--all", "1234"] = "tag-1234\n"
     `ls` # set the exit status code to 0
