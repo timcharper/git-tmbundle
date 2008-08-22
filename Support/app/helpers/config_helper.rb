1e85a9032525a3f34333766db05921baa699aa82
Diego Barros <diego@heyboyo.com>
2008-08-22 12:49:42 +1000

    - Local config settings for Log Limit and Log Context Lines were always saved to global and not saving to local.

--- Support/app/helpers/config_helper.rb
+++ Support/app/helpers/config_helper.rb
@@ -1,5 +1,5 @@
 module ConfigHelper
   def config_text_field(local_or_global, config_key, options = {})
-    content_tag :input, {:type => "text", :value => git.config[local_or_global, config_key], :onchange => "dispatch({controller: 'config', action: 'set', scope: 'global', key: '#{config_key}', value: $F(this)})"}.merge(options)
+    content_tag :input, {:type => "text", :value => git.config[local_or_global, config_key], :onchange => "dispatch({controller: 'config', action: 'set', scope: '#{local_or_global}', key: '#{config_key}', value: $F(this)})"}.merge(options)
   end
 end
