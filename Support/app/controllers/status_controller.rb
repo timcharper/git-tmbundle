5f379c78561f8b6e53a0588340792cec039bf197
Tim Harper <timcharper@gmail.com>
2008-09-02 18:33:21 -0600

    regression fix - dialogs were blowing up when no branch was selected

--- Support/app/controllers/status_controller.rb
+++ Support/app/controllers/status_controller.rb
@@ -2,7 +2,7 @@ class StatusController < ApplicationController
   include SubmoduleHelper
   def index
     file_or_path = params[:path] || git.paths.first
-    puts '<h2>Status for ' + file_or_path.map { |e| "‘#{htmlize(shorten(e))}’" }.join(', ') + " on branch ‘#{git.branch && git.branch.current.name}’</h2>"
+    puts '<h2>Status for ' + file_or_path.map { |e| "‘#{htmlize(shorten(e))}’" }.join(', ') + " on branch ‘#{git.branch.current_name}’</h2>"
     status_data = git.status(file_or_path)
     render "_status", :locals => {:status_data => status_data}
     
